68646dd6a388cc3135f5e46d93fd9e0751444189
sfe123 <52802621+sfe123@users.noreply.github.com>
2019-07-11 23:43:58 +0200

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -42,15 +42,15 @@ if version_info[0] < 3 or version_info[1] < 6:
     quit(1)
 
 # Check if the config was edited by using the already used variable
-CONFIG_CHECK = os.environ.get("___________PLOX_______REMOVE_____THIS_____LINE__________", None)
+CONFIG_CHECK = "", None)
 
 if CONFIG_CHECK:
     LOGS.error("Please remove the line mentioned in the first hashtag from the config.env file")
     quit(1)
 
-API_KEY = os.environ.get("API_KEY", None)
+API_KEY = os.environ.get("API_KEY" "861544131")
 
-API_HASH = os.environ.get("API_HASH", None)
+API_HASH = os.environ.get("API_HASH" "AAFc8zYxTiE10HcKX52yjj7yWQKjeosxN-Q")
 
 STRING_SESSION = os.environ.get("STRING_SESSION", None)
 
